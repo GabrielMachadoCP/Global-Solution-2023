8c9f04874bbf701a34ca38a4049fdb5de7c3d42c
bdancs <rm551645@fiap.com.br>
2023-11-22 03:53:54 -0300

    Muital alterações de scss, principalmente no header e página principal

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,49 +15,49 @@ export default function Home() {
         </section>
           
         {/* Parte direita da página */}
-        <div className="detalhes">
+        <section className="detalhes">
           {/* O que é a solução */}
-          <section>
+          <div>
             <h2>O que é a solução</h2>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis. Nostrum minus placeat quasi recusandae, possimus deleniti, ab ipsum totam aut autem odio a temporibus incidunt ex et consequatur pariatur obcaecati quam harum sed? Nisi, iste quam laboriosam tempore inventore quas nobis temporibus rem aut, eligendi error corporis pariatur suscipit?</p>
-          </section>
+          </div>
 
           {/* O que ela fará */}
-          <section>
+          <div>
             <h2>O que ela fará</h2>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis. Nostrum minus placeat quasi recusandae, possimus deleniti, ab ipsum totam aut autem odio a temporibus incidunt ex et consequatur pariatur obcaecati quam harum sed? Nisi, iste quam laboriosam tempore inventore quas nobis temporibus rem aut, eligendi error corporis pariatur suscipit?</p>
-          </section>
+          </div>
 
           {/* Como funcionará */}
-          <section>
+          <div>
             <h2>Como Funcionará</h2>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, omnis. Nostrum minus placeat quasi recusandae, possimus deleniti, ab ipsum totam aut autem odio a temporibus incidunt ex et consequatur pariatur obcaecati quam harum sed? Nisi, iste quam laboriosam tempore inventore quas nobis temporibus rem aut, eligendi error corporis pariatur suscipit?</p>
-          </section>
-        </div>
+          </div>
+        </section>
         
         {/* Cards de vantagens */}
-        <div className="vantagens">
-          <section>
+        <section className="vantagens">
+          <div>
             <h3>Vantagem 1</h3>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim architecto totam repudiandae reiciendis praesentium illum eos, eligendi at veniam quisquam.</p>
-          </section>
+            <p>Resposta mais rápida do socorro.</p>
+          </div>
 
-          <section>
+          <div>
             <h3>Vantagem 2</h3>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim architecto totam repudiandae reiciendis praesentium illum eos, eligendi at veniam quisquam.</p>
-          </section>
+            <p>Monitoramento 24h.</p>
+          </div>
 
-          <section>
+          <div>
             <h3>Vantagem 3</h3>
             
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim architecto totam repudiandae reiciendis praesentium illum eos, eligendi at veniam quisquam.</p>
-          </section>
-        </div>
+            <p>Evita perdas.</p>
+          </div>
+        </section>
       </main>
 
       <Rodape/>
